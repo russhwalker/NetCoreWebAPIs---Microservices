9c419abdc996633f1badf2e5293757bce0098a1d
russell walker <russ.h.walker@gmail.com>
2019-01-07 14:52:54 -0500

    adding new projects for base of web apps

---
+++ src/NetCoreWebAPIs.ReactWebApp/ClientApp/src/components/Login.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export class Login extends Component {
+    displayName = Login.name
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Login</h3>
+            </div>
+        );
+    }
+}
