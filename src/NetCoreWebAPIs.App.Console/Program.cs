98af6d00cd1d756d3d407f927e646f730da4dbc5
russell walker <russ.h.walker@gmail.com>
2019-01-11 07:27:32 -0500

    restructruing

---
+++ src/NetCoreWebAPIs.App.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace NetCoreWebAPIs.App.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+
+            var caller = new Core.APICaller();
+            var obj = new
+            {
+                userName = "uuuu",
+                password = "ppppp"
+            };
+            var token = caller.Post<JwtSecurityToken>("https://localhost:44360/api/Auth", obj);
+            //var result = caller.PostAsync<string>("https://localhost:44360/api/Auth", obj).Result;
+        }
+    }
+}
---
+++ src/NetCoreWebAPIs.Gateway/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace NetCoreWebAPIs.Gateway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        public string Post(string userName = "", string password = "")
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, userName)
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("abacawefwfasdfsd"));
+            var token = new JwtSecurityToken(
+                issuer: "netcorewebapisSite.com",
+                audience: "netcorewebapisSite.com",
+                expires: DateTime.Now.AddSeconds(5),
+                claims: claims,
+                signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature)
+                );
+            var x = new JwtSecurityTokenHandler();
+            return x.WriteToken(token);
+            //return $"u:{userName};p:{password}";
+        }
+    }
+}
